736ef47ab134212b0049e936abde78f84990f28b
yyyoichi <origamiichiro@gmail.com>
2024-10-04 23:28:46 +0900

    feat 相対位置と位置計算実装

--- internal/core/ocean.go
+++ internal/core/ocean.go
@@ -4,13 +4,45 @@ var DefaultOceanMap = OceanMap{
 	Id:          201,
 	H:           6,
 	W:           6,
-	IsLandCount: 2,
+	IslandCount: 2,
 }
 
 // 対戦ボード
 type OceanMap struct {
-	Id   uint8
-	H, W uint8
+	Id   int32
+	H, W int8
 	// 島数
-	IsLandCount uint8
+	IslandCount int8
+}
+
+// atからの相対位置rssを絶対位置で返す。
+func (om OceanMap) Sectors(at Sector, rss RelativeSectors) []Sector {
+	var resp = make([]Sector, 0, len(rss))
+	atX, atY := int8(at)%om.W, int8(at)/om.W
+	for _, s := range rss {
+		x := atX + s[0]
+		if x < 0 || om.W <= x {
+			continue
+		}
+		y := atY + s[1]
+		if y < 0 || om.H <= y {
+			continue
+		}
+		resp = append(resp, Sector(om.W*y+x))
+	}
+	return resp
+}
+
+// 2次元スライスで区域を返す。
+// ex) [[0,1,2], [3,4,5], [6,7,8]]
+func (om OceanMap) Lines() [][]Sector {
+	var resp = make([][]Sector, om.H)
+	for y := range om.H {
+		line := make([]Sector, om.W)
+		for x := range om.W {
+			line[x] = Sector(om.W*y + x)
+		}
+		resp[y] = line
+	}
+	return resp
 }
--- internal/core/sector.go
+++ internal/core/sector.go
@@ -1,7 +1,33 @@
 package core
 
 type (
-	Sector          uint8
-	Sectors         []Sector
+	Sector int8
+	// 相対位置[x,y]
 	RelativeSectors [][2]int8
 )
+
+// 相対位置の方向を転換する
+func (rss RelativeSectors) Trun(d Direction) RelativeSectors {
+	var f func([2]int8) [2]int8
+	switch d {
+	case North:
+		f = func(i [2]int8) [2]int8 { return i }
+	case East:
+		f = func(i [2]int8) [2]int8 {
+			return [2]int8{i[1] * -1, i[0]}
+		}
+	case South:
+		f = func(i [2]int8) [2]int8 {
+			return [2]int8{i[0] * -1, i[1] * -1}
+		}
+	case West:
+		f = func(i [2]int8) [2]int8 {
+			return [2]int8{i[1], i[0] * 1}
+		}
+	}
+	var resp = make([][2]int8, len(rss))
+	for i, s := range rss {
+		resp[i] = f(s)
+	}
+	return RelativeSectors(resp)
+}
