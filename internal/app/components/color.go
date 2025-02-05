d946c5f28157a46074968af8b8a8e2eccd991585
yyyoichi <origamiichiro@gmail.com>
2025-02-06 00:36:05 +0900

    fix: color A

--- internal/app/components/color.go
+++ internal/app/components/color.go
@@ -3,7 +3,7 @@ package components
 import "image/color"
 
 var (
-	Orange = color.NRGBA{226, 123, 10, 1}
-	Red    = color.NRGBA{190, 40, 22, 1}
-	Green  = color.NRGBA{22, 150, 63, 1}
+	Orange = color.NRGBA{226, 123, 10, 255}
+	Red    = color.NRGBA{190, 40, 22, 255}
+	Green  = color.NRGBA{22, 150, 63, 255}
 )
