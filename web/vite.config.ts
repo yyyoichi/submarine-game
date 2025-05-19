e1ba4752a1c7873dd538a5902a30f2c03b30dfdc
yyyoichi <origamiichiro@gmail.com>
2025-05-19 20:14:20 +0900

    add tailwind and shadcn/ui

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/rpc": {
+        // @ts-ignore
         target: `http://localhost:${process.env.PORT}`,
         changeOrigin: true,
       },
     }
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
