b2474ae60610bc841bfd34504ce859b31c89779d
yyyoichi <origamiichiro@gmail.com>
2025-05-24 12:31:26 +0900

    add props maker without webapi dependency

--- web/app/routes/debug/playground.tsx
+++ web/app/routes/debug/playground.tsx
@@ -1,60 +1,32 @@
-import type React from "react";
-import { useEffect, useState } from "react";
 import { useSubmit } from "react-router";
 import { PreparingPage } from "../playgrounds/component";
+import { usePreparingPageProps } from "../playgrounds/props";
 
+const islands = [
+  Math.floor(Math.random() * 6 ** 2),
+  Math.floor(Math.random() * 6 ** 2),
+];
 export default function Playground() {
   const submit = useSubmit();
-  const [viewGuide, setViewGuide] = useState(false);
-  const [step, setStep] = useState<1 | 2 | 3>(1);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setStep((v) => {
-        if (v === 3) {
-          return 1;
-        }
-        return (v + 1) as 2 | 3;
-      });
-    }, 1000 * 10);
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-  const props: React.ComponentProps<typeof PreparingPage> = {
-    // @ts-ignore
-    preparingStep: ["me", "mines", "done"][step - 1],
-    Leading: {
-      Alerm: {
-        useAlerm: true,
-        startPingSec: 30,
-        finishDatetime: new Date(Date.now() + 1000 * 30),
-      },
+  const enables: number[] = [];
+  for (let i = 0; i < 6 ** 2; i++) {
+    if (islands.includes(i)) {
+      continue;
+    }
+    enables.push(i);
+  }
+  const props = usePreparingPageProps({
+    timeout: new Date(Date.now() + 1000 * 60), // 60s
+    milliSecondPerTurn: 1000 * 30, // 30s
+    boardWidth: 6,
+    mineCount: 2,
+    islandSectors: islands,
+    enableDeploySectors: enables,
+    enableMineSectors: enables,
+    deployAction: ({ deployAt, mines }) => {
+      submit({ deployAt, mines }, { method: "post" });
     },
-    Ocean: {
-      sectorCount: 6,
-      islands: [10, 28],
-    },
-    MeOcean: {
-      sectors: [12],
-    },
-    MinesOcean: {
-      sectors: [18, 32],
-    },
-    Controller: {
-      Direction: {
-        North: {},
-        West: {},
-        South: {},
-        East: {},
-      },
-      AButton: {
-        onClick: () => {
-          submit({}, { method: "POST" });
-        },
-      },
-      BButton: {},
-    },
-  };
+  });
   return <PreparingPage {...props} />;
 }
 
---
+++ web/app/routes/playgrounds/props.ts
@@ -0,0 +1,209 @@
+import { useState } from "react";
+import type { PreparingPage } from "./component";
+
+interface PreparingPageParams {
+  timeout: Date;
+  milliSecondPerTurn: number;
+  boardWidth: number;
+  mineCount: number;
+
+  islandSectors: number[];
+  enableDeploySectors: number[];
+  enableMineSectors: number[];
+
+  deployAction: (param: { deployAt: number; mines: number[] }) => void;
+}
+
+type Props = React.ComponentProps<typeof PreparingPage>;
+
+type SectorStates =
+  | {
+      state: "selectme";
+      cursorSct: number;
+    }
+  | {
+      state: "selectmines";
+      cursorSct: number;
+      selectedMeSct: number;
+      selectedMineScts: number[];
+    }
+  | {
+      state: "done";
+      selectedMeSct: number;
+      selectedMineScts: number[];
+    };
+
+export const usePreparingPageProps = (p: PreparingPageParams) => {
+  const randSct = () =>
+    p.enableDeploySectors[
+      Math.floor(Math.random() * p.enableDeploySectors.length)
+    ];
+  const [sctState, setSctState] = useState<SectorStates>({
+    state: "selectme",
+    cursorSct: randSct(),
+  });
+
+  const directionAction = (dir: "North" | "West" | "South" | "East") => {
+    switch (sctState.state) {
+      case "selectme": {
+        console.log("me move to", dir);
+        break;
+      }
+      case "selectmines": {
+        console.log("mines move to", dir);
+        break;
+      }
+    }
+  };
+  const aAction = () => {
+    if (sctState.state === "done") {
+      return;
+    }
+    setSctState((prev) => {
+      switch (prev.state) {
+        case "selectme": {
+          if (!p.enableDeploySectors.includes(prev.cursorSct)) {
+            return prev;
+          }
+          return {
+            state: "selectmines",
+            cursorSct: randSct(),
+            selectedMeSct: prev.cursorSct,
+            selectedMineScts: [],
+          };
+        }
+        case "selectmines": {
+          if (!p.enableMineSectors.includes(prev.cursorSct)) {
+            return prev;
+          }
+          if (prev.selectedMineScts.length >= p.mineCount) {
+            return prev;
+          }
+          if (prev.selectedMineScts.length + 1 !== p.mineCount) {
+            return {
+              state: "selectmines",
+              cursorSct: randSct(),
+              selectedMeSct: prev.selectedMeSct,
+              selectedMineScts: [prev.cursorSct, ...prev.selectedMineScts],
+            };
+          }
+          // 最後の一つ state更新,deploy
+          p.deployAction({
+            deployAt: prev.selectedMeSct,
+            mines: [...prev.selectedMineScts, prev.cursorSct],
+          });
+          return {
+            state: "done",
+            selectedMeSct: prev.selectedMeSct,
+            selectedMineScts: [...prev.selectedMineScts, prev.cursorSct],
+          };
+        }
+      }
+      return prev;
+    });
+  };
+  const bAction = () => {
+    if (sctState.state === "done") {
+      return;
+    }
+    setSctState((prev) => {
+      switch (prev.state) {
+        case "selectme": {
+          return {
+            state: "selectme",
+            cursorSct: randSct(),
+          };
+        }
+        case "selectmines": {
+          if (!prev.selectedMineScts.length) {
+            return {
+              state: "selectme",
+              cursorSct: prev.selectedMeSct,
+            };
+          }
+          const [last, ...scts] = prev.selectedMineScts;
+          return {
+            state: "selectmines",
+            cursorSct: last,
+            selectedMeSct: prev.selectedMeSct,
+            selectedMineScts: scts,
+          };
+        }
+      }
+      return prev;
+    });
+  };
+
+  const displayMeSectors = (): number[] => {
+    switch (sctState.state) {
+      case "selectme": {
+        return [sctState.cursorSct];
+      }
+      default: {
+        return [sctState.selectedMeSct];
+      }
+    }
+  };
+  const displayMineSectors = (): number[] => {
+    switch (sctState.state) {
+      case "selectme": {
+        return [];
+      }
+      case "selectmines": {
+        return [...sctState.selectedMineScts, sctState.cursorSct];
+      }
+      case "done":
+        return sctState.selectedMineScts;
+    }
+  };
+
+  const props: Props = {
+    preparingStep:
+      sctState.state === "selectme"
+        ? "me"
+        : sctState.state === "selectmines"
+          ? "mines"
+          : "done",
+    Leading: {
+      Alerm: {
+        useAlerm: true,
+        startPingMilliSec: p.milliSecondPerTurn,
+        finishDatetime: p.timeout,
+      },
+    },
+    Ocean: {
+      sectorCount: p.boardWidth,
+      islands: p.islandSectors,
+    },
+    MeOcean: {
+      sectors: displayMeSectors(),
+    },
+    MinesOcean: {
+      sectors: displayMineSectors(),
+    },
+    Controller: {
+      Direction: {
+        North: {
+          onClick: () => directionAction("North"),
+        },
+        West: {
+          onClick: () => directionAction("West"),
+        },
+        South: {
+          onClick: () => directionAction("South"),
+        },
+        East: {
+          onClick: () => directionAction("East"),
+        },
+      },
+      AButton: {
+        onClick: sctState.state === "done" ? undefined : aAction,
+      },
+      BButton: {
+        onClick: sctState.state === "done" ? undefined : bAction,
+      },
+    },
+  };
+
+  return props;
+};
