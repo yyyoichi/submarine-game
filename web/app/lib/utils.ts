55be588aafca1d70e323c595b5076d657d365c03
yyyoichi <origamiichiro@gmail.com>
2025-05-29 21:44:21 +0900

    chore transpose view sector name

--- web/app/lib/utils.ts
+++ web/app/lib/utils.ts
@@ -6,13 +6,17 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 /**
- * A1, A2, A3, A4, ...D1, D2, D3, D4 のようにセクター位置を文字に変換する
+ * A1, B1, C1, D1, ...
+ * ....
+ * A6, B6, C6, D6
+ *
+ *  のようにセクター位置を文字に変換する
  * @param base 1辺のセクターの数
  * @param i 左上から数えたセクターのインデックス
  * @returns セクター位置を表す文字列
  */
 export function sector(base: number, i: number) {
-  const x = Math.floor(i / base);
-  const y = i % base;
+  const x = i % base;
+  const y = Math.floor(i / base);
   return String.fromCharCode(65 + x) + (y + 1);
 }
